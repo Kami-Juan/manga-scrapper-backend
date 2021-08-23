86a4ed63cd0cf24ee9ef3367d6f503ad234a9511
KamiJohn <kami@rxrise.com>
2021-08-23 12:54:57 -0500

    db: added new seeds

--- packages/backend/prisma/seed.ts
+++ packages/backend/prisma/seed.ts
@@ -5,12 +5,30 @@ const prisma = new PrismaClient();
 
 async function main() {
   const kamiPassword = await bcrypt.hash('Watusi04', 10);
+  const sofitoonsPassword = await bcrypt.hash('Sofitoons05', 10);
+  const karolPassword = await bcrypt.hash('Karol001', 10);
 
   const kami = await prisma.user.create({
     data: {
       email: `juaneldios007@live.com.mx`,
       username: 'Kamiganzo',
       password: kamiPassword,
+    },
+  });
+
+  const sofitoons = await prisma.user.create({
+    data: {
+      email: `sofitoons@hotmail.com`,
+      username: 'SofiToons',
+      password: sofitoonsPassword,
+    },
+  });
+
+  const karol = await prisma.user.create({
+    data: {
+      email: `karol@hotmail.com`,
+      username: 'Karol',
+      password: karolPassword,
     },
   });
 }
